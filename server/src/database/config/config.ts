93ae08139e1ce7e188e8da74f033c56df8d7cbe5
Justin True <justin.d.true@wmich.edu>
2018-04-03 17:36:25 -0400

    config

---
+++ server/src/database/config/config.ts
@@ -0,0 +1,23 @@
+export default {
+    'development': {
+      'username': 'u306130368_sch',
+      'password': 'Bennett00',
+      'database': 'u306130368_sch',
+      'host': '	mysql.hostinger.com',
+      'dialect': 'mysql'
+    },
+    'test': {
+      'username': 'postgres',
+      'password': 'Bennett00',
+      'database': 'incidenttracking',
+      'host': 'localhost',
+      'dialect': 'postgres'
+    },
+    'production': {
+      'username': 'postgres',
+      'password': 'Bennet',
+      'database': 'incidenttracking',
+      'host': 'localhost',
+      'dialect': 'postgres'
+    }
+  };
